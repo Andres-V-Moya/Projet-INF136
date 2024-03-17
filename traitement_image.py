aa64d81896b68df8fae9d7df2ceb5ccd6b4e1458
JP1516 <jeremy.plante.1@ens.etsmtl.ca>
2024-03-17 12:15:41 -0400

    Debut traitement image

--- traitement_image.py
+++ traitement_image.py
@@ -4,18 +4,26 @@ import matplotlib.pyplot as plt
 
 def appliquer_rgb_to_gry(chemin_vers_image_org, chemin_vers_image_ng):
 
+    # Chargement de l'image originale
     img = plt.imread(chemin_vers_image_org)
 
+    # Dimension de l'image a traiter
     height, width, _ = img.shape
 
+    # Creation d'un tableau ayant les memes dimensions que l'image et ayant les trois canaux de couleur par pixel.
     gry_img = np.zeros((height, width, 3), dtype=np.uint8)
 
+    # Conversion des couleurs RGB en niveau de gris
     for y in range(height):
         for x in range(width):
-            # Calculate average of RGB components
+            # Calcul de la moyenne des couleurs RGB
             gray_value = np.mean(img[y, x])
 
-            # Assign grayscale value to each pixel
+            # Attribution des niveau de gris a la nouvelle image
             gry_img[y, x] = gray_value
 
+    # Enregistrement de l'image
     plt.imsave(chemin_vers_image_ng, gry_img, cmap='gray')
+
+
+def appliquer_transformation_1() :
