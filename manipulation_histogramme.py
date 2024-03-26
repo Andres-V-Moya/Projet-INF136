bbd84eac0e1a129b49fb1523bcbeb64637504b42
JP1516 <jeremy.plante.1@ens.etsmtl.ca>
2024-03-25 21:50:55 -0400

    mapipulation histogramme 2

--- manipulation_histogramme.py
+++ manipulation_histogramme.py
@@ -1,38 +1,37 @@
 import numpy as np
 import math
 
-def petit_tableau(tableau, i, j, sub_array_size):
-    max_value = tableau.max()
-    m, n = tableau.shape
-    half_size = math.ceil(sub_array_size / 2)
+def petit_tableau(tableau, i, j, w):
 
-    # Generate indices for the sub-array
-    indices = [(x, y) for x in range(i - half_size, i + half_size)
-               for y in range(j - half_size, j + half_size)
+    valeur_max = tableau.max()
+    m, n = tableau.shape
+
+    # coordonne des valeurs du petit tableau
+    indices = [(x, y) for x in range(i - w, i)
+               for y in range(j - w, j)
                if 0 <= x < m and 0 <= y < n]
 
-    # Extract values from the larger array using the generated indices
+    # Extraction des valeurs dans le petit tableau
     ptit_tableau = [tableau[x, y] for x, y in indices]
 
-    hist, _ = np.histogram(ptit_tableau, bins=[0, max_value / 4, max_value / 2, (3 * max_value) / 4, max_value],
-                           range=(0, max_value))
+    # Histogramme des valeurs extraites
+    hist, _ = np.histogram(ptit_tableau, bins=[0, valeur_max / 4, valeur_max / 2, (3 * valeur_max) / 4, valeur_max],
+                           range=(0, valeur_max))
 
     return hist
 
-def calculer_histogramme(tableau, sub_array_size):
-    """
-    Calcul de la valeur entière correspondant au motif binaire pour chaque valeur du tableau.
-    """
+def calculer_histogramme(tableau, w):
+
     m, n = tableau.shape
     resultat = []
 
     # Parcours du tableau pour chaque valeur
-    for i in range(m):
-        for j in range(n):
-            resultat.append(petit_tableau(tableau, i, j, sub_array_size))
+    for i in range(w, m + 1):
+        for j in range(w, n + 1):
+            resultat.append(petit_tableau(tableau, i, j, w))
 
     resultat = np.array(resultat)
-    print(resultat)
+    return resultat
 
 def calculer_distance_1(histo1, histo2):
     distance = np.sum((histo1 - histo2) ** 2)
@@ -46,14 +45,3 @@ def calculer_distance_2(histo1, histo2):
     distance = np.sum((abs(histo1 - histo2)))
 
     return round(distance, 2)
-
-
-if __name__ == '__main__':
-    tableau = np.array([[255, 160, 10, 49],
-                        [20, 170, 1, 121],
-                        [30, 233, 230, 100],
-                        [255, 23, 155, 88]])
-
-    sub_array_size = 2  # Define the size of the sub-arrays
-
-    calculer_histogramme(tableau, sub_array_size)
